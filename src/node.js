522161b028d8c14e9d8730801c995d53f2b7bc31
Glowzer <31700869+Glowzer@users.noreply.github.com>
2017-10-29 16:18:31 +0300

    node finished

--- src/node.js
+++ src/node.js
@@ -1,23 +1,71 @@
 class Node {
-	constructor(data, priority) {
+  constructor(data, priority) {
+    this.data = data;
+    this.priority = priority;
+    this.parent = null;
+    this.left = null;
+    this.right = null;
+  }
 
-	}
+  appendChild(node) {
+    if (node !== null) {
+      if (this.left == null) {
+        this.left = node;
+        node.parent = this;
+      } else if (this.right == null) {
+        this.right = node;
+        node.parent = this;
+      }
+    }
+  }
 
-	appendChild(node) {
+  removeChild(node) {
+    if (this.left == node) {
+      this.left = null;
+      node.parent = null;
+    } else if (this.right == node) {
+      this.right = null;
+      node.parent = null;
+    } else throw new Error();
+  }
 
-	}
+  remove() {
+    if (this.parent !== null) {
+      this.parent.removeChild(this);
+    }
+  }
 
-	removeChild(node) {
+  swapWithParent() {
+    if (this.parent !== null) {
+      var father = this.parent;
+      var gfather = this.parent.parent;
 
-	}
+      var lflag = false;
+      var save_child = father.left;
+      if (father.left == this) {
+        lflag = true;
+        save_child = father.right;
+      }
 
-	remove() {
+      this.remove();
+      father.remove();
 
-	}
+      if (gfather !== null)
+        gfather.appendChild(this);
+      father.left = father.right = null;
+      father.appendChild(this.left);
+      father.appendChild(this.right);
 
-	swapWithParent() {
-		
-	}
+      this.left = this.right = null;
+      if (lflag) {
+        this.appendChild(father);
+        this.appendChild(save_child);
+      } else {
+        this.appendChild(save_child);
+        this.appendChild(father);
+      }
+    }
+  }
 }
 
 module.exports = Node;
