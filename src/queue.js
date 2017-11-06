fcc88fb7a5a38e1c3251048a4cfd9a355f86e00f
Glowzer <31700869+Glowzer@users.noreply.github.com>
2017-11-06 23:37:49 +0300

    final commit

--- src/queue.js
+++ src/queue.js
@@ -1,25 +1,35 @@
 const MaxHeap = require('./max-heap.js');
 
 class PriorityQueue {
-	constructor(maxSize) {
+  constructor(maxSize) {
+    if (maxSize == null) {
+      this.maxSize = 30;
+    } else {
+      this.maxSize = maxSize;
+    }
+    this.heap = new MaxHeap();
+  }
 
-	}
+  push(data, priority) {
+    if (this.heap.size() < this.maxSize)
+      this.heap.push(data, priority);
+    else
+      throw new Error();
+  }
 
-	push(data, priority) {
+  shift() {
+    if (this.heap.isEmpty())
+      throw new Error();
+    return this.heap.pop();
+  }
 
-	}
+  size() {
+    return this.heap.size();
+  }
 
-	shift() {
-
-	}
-
-	size() {
-
-	}
-
-	isEmpty() {
-		
-	}
+  isEmpty() {
+    return this.heap.isEmpty();
+  }
 }
 
 module.exports = PriorityQueue;
